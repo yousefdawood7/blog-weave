64d8cffa4397f9f86994c8a1ebb89e08e2933d91
Yousef Dawood <yousefdawood31@gmail.com>
2026-03-25 23:34:11 +0200

    refactor TypeScript configuration and update Vite alias resolution

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
