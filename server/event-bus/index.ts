1a44391bf3052119e242e470ba32c69e4fae0abf
Yousef Dawood <yousefdawood31@gmail.com>
2026-03-31 04:46:13 +0200

    setup event's bus middlewars

---
+++ server/event-bus/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
