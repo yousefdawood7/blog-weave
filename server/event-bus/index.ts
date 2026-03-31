fd1e4245e381abbb096131e1f9fa8e9f7e1657b7
Yousef Dawood <yousefdawood31@gmail.com>
2026-03-31 04:51:06 +0200

    add event bus endpoint to forward events to all existing services

--- server/event-bus/index.ts
+++ server/event-bus/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import helmet from "helmet";
 import cors from "cors";
+import axios from "axios";
 
 const app = express();
 
@@ -8,3 +9,14 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.post("/event", (req, res) => {
+  axios.post("localhost:4000/event", req.body);
+  axios.post("localhost:4001/event", req.body);
+
+  res.send({ message: "Forwards event to all our services" });
+});
+
+app.listen(4005, () => {
+  console.log("Server is running on port 4000");
+});
