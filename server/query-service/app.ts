92a8d41bcf2144b23946bce62c953ed1ed533f80
Yousef Dawood <yousefdawood31@gmail.com>
2026-04-07 06:42:42 +0200

    use es-build as our bundler

---
+++ server/query-service/app.ts
@@ -0,0 +1,19 @@
+import express, { type Express } from "express";
+import helmet from "helmet";
+import cors from "cors";
+
+import postsRouter from "./routes/posts.routes";
+import eventRouter from "./routes/event.routes";
+
+const app: Express = express();
+
+app.use(cors());
+
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/posts", postsRouter);
+app.use("/events", eventRouter);
+
+export default app;
