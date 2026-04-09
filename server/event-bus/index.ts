dfd389b1aa3ef830539f08068faf4caebe38b49a
Yousef Dawood <yousefdawood31@gmail.com>
2026-04-10 00:21:32 +0200

    store our event that emitted from event bus

--- server/event-bus/index.ts
+++ server/event-bus/index.ts
@@ -10,18 +10,30 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+type EventType = {
+  type: string;
+  payload: { [key: string]: any };
+};
+
+const events: EventType[] = [];
+
 app.post("/event", async (req, res) => {
+  const event = req.body;
+  events.push(event);
+
   await Promise.all([
-    axios.post("http://localhost:4000/event", req.body),
-    axios.post("http://localhost:4001/event", req.body),
-    axios.post("http://localhost:4002/event", req.body),
-    axios.post("http://localhost:4003/event", req.body),
+    axios.post("http://localhost:4000/event", event),
+    axios.post("http://localhost:4001/event", event),
+    axios.post("http://localhost:4002/event", event),
+    axios.post("http://localhost:4003/event", event),
   ]);
 
   res
     .status(200)
     .send({ status: 200, message: "Forwards event to all our services" });
 });
+
+app.get("/event", (_req, res) => res.json(events));
 
 app.listen(4005, () => {
   console.log("Server is running on port 4005");
