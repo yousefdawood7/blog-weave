098af3e0da3513d4fd0bb1e7446e26b8faf64118
Yousef Dawood <yousefdawood31@gmail.com>
2026-04-18 00:10:05 +0200

    update event bus service URLs to use cluster IPs instead of localhost

--- server/event-bus/index.ts
+++ server/event-bus/index.ts
@@ -24,8 +24,8 @@ app.post("/event", async (req, res) => {
   await Promise.all([
     axios.post("http://posts-cluster-ip-srv:4000/event", event),
     axios.post("http://comments-cluster-ip-srv:4001/event", event),
-    axios.post("http://localhost:4002/event", event),
-    axios.post("http://localhost:4003/event", event),
+    axios.post("http://query-cluster-api-srv:4002/event", event),
+    axios.post("http://moderation-cluster-ip-srv:4003/event", event),
   ]);
 
   res
